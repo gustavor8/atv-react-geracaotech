09b36e65b1ff9c6609b5683f86c35c276dc675c1
Gustavo Rodrigues <gustavogrmc@gmail.com>
2025-05-08 13:20:30 -0300

    Fit: Ajuste Routes, add Suspense tag

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -7,11 +7,13 @@ const About = lazy(() => import("./pages/about/about"));
 const Home = lazy(() => import("./pages/home/home"));
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
